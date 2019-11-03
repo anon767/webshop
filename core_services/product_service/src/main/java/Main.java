e162930cd9702ab74351ef1eb161e69b9232ae64
tganz <tganz@inovex.de>
2019-11-03 14:09:39 +0100

    added product service stub

---
+++ core_services/product_service/src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.hska.webshop.product.api.ProductApi;
+import com.hska.webshop.product.api.model.Product;
+public class Main {
+    public static void main(String[] args) {
+        Product product = new Product();
+    }
+}
