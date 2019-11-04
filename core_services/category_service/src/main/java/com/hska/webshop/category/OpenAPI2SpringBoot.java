66bdc9a90e569e61eac5eb103a16c1a3ac816020
tganz <tganz@inovex.de>
2019-11-04 11:41:52 +0100

    properties fix

--- core_services/category_service/src/main/java/com/hska/webshop/category/OpenAPI2SpringBoot.java
+++ core_services/category_service/src/main/java/com/hska/webshop/category/OpenAPI2SpringBoot.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.hska.webshop.category", "org.openapitools.api" , "com.hska.webshop.category.config"})
